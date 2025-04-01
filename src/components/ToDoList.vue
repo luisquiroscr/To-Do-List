3ed4518ca0c86d96b3edfbf971ffcd9814328d3c
luisquiroscr <lquiros@vlacademy.com>
2025-03-31 23:26:20 -0600

    Creación Repositorio TODOLIST
    Se crea de manera local y se exporta a la nube el Repositorio TO-DO-LIST trabajado en clase

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,76 @@
+<template>
+  <h1>{{ titulo1 }}</h1>
+
+  <div class="formulario">
+    <input v-model="tareaNueva" placeholder="Escribe una tarea" />
+    <button v-on:click="agregarElemento">Agregar</button>
+  </div>
+
+  <h2>Tareas Pendientes</h2>
+  <ul>
+    <li v-for="(tarea, index) in listaDeTareas" :key="index">
+      {{ index }} - {{ tarea }}
+      <button @click="eliminarElemento(index)">Eliminar</button>
+    </li>
+  </ul>
+
+  <h2>Tareas Completadas</h2>
+  <p v-if="contadorDeTareasCompletadas === 0">
+    <span>No hay tareas completadas</span>
+  </p>
+  <ul v-else style="min-height: 100px; background-color: green;">
+    <li v-for="(tarea, index) in tareasCompletadas" :key="index">
+      {{ index }} - {{ tarea }}
+    </li>
+  </ul>
+
+  <h2>Resumen de tareas</h2>
+  <p>Completadas: {{ contadorDeTareasCompletadas }} | Pendientes: {{ tareasPendientes }}</p>
+  <p>Total de tareas: {{ totalDeTareas }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo1: 'Aprendizaje de Vue',
+      listaDeTareas: [],
+      tareaNueva: '',
+      tareasCompletadas: []
+    };
+  },
+
+  computed: {
+    tareasPendientes() {
+      return this.listaDeTareas.length;
+    },
+    contadorDeTareasCompletadas() {
+      return this.tareasCompletadas.length;
+    },
+    totalDeTareas() {
+      return this.listaDeTareas.length + this.tareasCompletadas.length;
+    }
+  },
+
+  methods: {
+    agregarElemento() {
+      if (this.tareaNueva.trim() !== '') {
+        this.listaDeTareas.push(this.tareaNueva.trim());
+        this.tareaNueva = '';
+      }
+    },
+    eliminarElemento(index) {
+      this.tareasCompletadas.push(this.listaDeTareas[index]);
+      this.listaDeTareas.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.formulario {
+   display: block;
+   background-color: red;
+ }
+
+</style>
